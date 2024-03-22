e19f6ddfc8c585e6691d65820f58ff81253d7b62
Ahmed Noorim <ahhmed.noorim13@gmail.com>
2024-03-23 02:00:51 +0600

    ui -3

--- lib/utilities/notes.dart
+++ lib/utilities/notes.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class NotesPage extends StatefulWidget {
@@ -11,8 +13,61 @@ class _NotesPageState extends State<NotesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Notes"),
+      backgroundColor: Colors.transparent,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // card container
+          // _cardContainer(),
+          Expanded(
+            child: Container(
+              height: 100,
+              decoration: BoxDecoration(
+                color: Color(0xFFD9CAB3),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, bottom: 5),
+                    child: Text(
+                      "Notes",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: "Poppins",
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: 10,
+                        right: 10,
+                        bottom: 10,
+                        top: 10,
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: TextField(
+                            maxLines: null,
+                            decoration: InputDecoration.collapsed(
+                              hintText: "Enter your text here",
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
